ed39756049e74ef910415e513b23239fd750ac8a
Ian Shih <ianshih22856@gmail.com>
2022-09-30 16:12:24 -0700

    tempcode

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main (){
-    cout << "\"hi\"";
-    string testHi;
-    testHi = "Hi";
-    cout << testHi;
-    long long x = 100000000929299292;
-    cout << x;
-}   
+int main(){}
