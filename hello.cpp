4936b26bd2a0be3e4a48ef1b1fa175179b65e554
Ian Shih <ianshih22856@gmail.com>
2022-10-24 14:43:53 -0700

    random

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,87 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 	using namespace std;
 
 	int main()
 	{
-		int a = 1;
-	  for (int i = 1; i <= 5; ++i){
-		cout << i;
+		// 1. 1
+		// 2. *
+		// 2. **
+		// 2. 3,4,6
+		// 3. 7
+		// 4. 4
+		// 5. 1
+		// 6. 4
+		// 7.
+		// int score = 0;
+		// cout << "Please enter the student's score:";
+		// cin >> score;
 
-	  }
+		// int grade;
+		// if (score >= 90 && score <= 100)
+		// {
+		// 	grade = 4;
+		// }
+		// else if (score >= 80)
+		// {
+		// 	grade = 3;
+		// }
+		// else if (score >= 70)
+		// {
+		// 	grade = 2;
+		// }
+		// else if (score >= 60)
+		// {
+		// 	grade = 1;
+		// }
+		// else 
+		// {
+		// 	grade = 0;
+		// }
+		// switch (grade)
+		// {
+		// 	case 4:
+		// 		cout << "A" << endl;
+		// 		break;
+		// 	case 3:
+		// 		cout << "B" << endl;
+		// 		break;
+		// 	case 2:
+		// 		cout << "C" << endl;
+		// 		break;
+		// 	case 1:
+		// 		cout << "D" << endl;
+		// 		break;
+		// 	case 0:
+		// 		cout << "F" << endl;
+		// 		break;
+		// } 
+		// 8. 
+		int i;
+		string str;
+		cout << "enter: " << endl;
+		cin >> str;
+
+		i = 0;
+		char lowercase = '*';
+		do 
+		{
+			if(islower(str[i]))
+			{
+				break;
+			}
+			i++;
+		}
+		while(i < str.size());
+
+		if (i == str.size())
+		{
+			cout << "No lowercase character." << endl;
+		}
+		else
+		{
+			cout << str[i] << endl;
+		}
+		return 0;
 	}
--- randomstuff.cpp
+++ randomstuff.cpp
@@ -8,31 +8,22 @@
 #include <math.h>
 using namespace std;
 
-string ReverseString(string word)
+int main()
 {
-    string codeSection;
-    cout << "enter codesection: ";
-    getline(cin, codeSection);
-    int test = stoi(codeSection);
-    cout << test << endl;
+    int test;
+    cout << "enter: " << endl;
+    cin >> test;
+    switch(test)
+    {
+        case 1:
+            cout << "1" << endl;
+        case 2:
+            cout << "2" << endl;
 
-    for (int i = 0; i < userInput.length(); i++)
-    {
-        string temp(1, userInput.at(i));
-        if (temp == " ")
-        {
-            userInput.replace(i, 1, "");
-        }
-    }
-    string reversedWord = ReverseString(userInput);
-    if (reversedWord == userInput)
-    {
-        cout << "palindrome: " << original << endl;
-    }
-    else
-    {
-        cout << "not a palindrome: " << original << endl;
-    }
+        case 3:
+            cout << "3" << endl;
 
-    return 0;
+        default:
+            cout << "0" << endl;
+    }
 }
