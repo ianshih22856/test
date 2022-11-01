a4f3ad8dd92212f3289d6558eff598d94d9d5954
Ian Shih <ianshih22856@gmail.com>
2022-11-01 16:13:03 -0700

    arraytests1

--- array.cpp
+++ array.cpp
@@ -60,7 +60,11 @@ int positionOfMax(const string a[], int n)
 
 int rotateLeft(string a[], int n, int pos)
 {
-    if (!CheckIntParam(n))
+    if (!CheckIntParam(n) || !CheckIntParam(pos))
+    {
+        return -1;
+    }
+    if (pos >= n)
     {
         return -1;
     }
@@ -163,6 +167,7 @@ int subsequence(const string a1[], int n1, const string a2[], int n2)
         {
             if (a2[0] == a1[i])
             {
+                // a1 does not contain a2 and n2 is not an empty array
                 if (i == n1 - 1 && n2 > 1)
                 {
                     n = -1;
@@ -245,4 +250,63 @@ int split(string a[], int n, string splitter)
 
 int main()
 {
+    string h[9] = {"rishi", "margaret", "gordon", "tony", "", "john", "liz","gordon", "joe"};
+    
+    // assert(lookup(h, 7, "john") == 5);
+    // assert(lookup(h, 8, "gordon") == 2);
+    // assert(lookup(h, 2, "gordon") == -1);
+    // assert(lookup(h, 3, "rishi") == 0);
+    // assert(lookup(h, 9, "joe") == 8);
+    // assert(lookup(h, 0, "joe") == -1);
+    // assert(lookup(h, -2, "joe") == -1);
+
+    string v[9] = {"", "margaret", "tony", "tony", "", "john", "liz", "gordon", "zeus"};
+
+    // assert(positionOfMax(h, 7) == 3);
+    // assert(positionOfMax(h, 0) == 0);
+    // assert(positionOfMax(h, 3) == 0);
+    // assert(positionOfMax(h, 3) == 0);
+
+    // assert(positionOfMax(v, 1) == 0);
+    // assert(positionOfMax(v, 9) == 8);
+    // assert(positionOfMax(v, 5) == 2);
+    // assert(positionOfMax(v, -2) == -1);
+
+    string g[4] = {"rishi", "margaret", "liz", "theresa"};
+    // assert(rotateLeft(g, 4, 1) == 1 && g[1] == "liz" && g[3] == "margaret");
+    // assert(rotateLeft(g, 3, 1) == 1 && g[1] == "liz" && g[3] == "theresa");
+    // assert(rotateLeft(g, -2, 1) == -1);
+    // assert(rotateLeft(g, 1, -1) == -1);
+    // assert(rotateLeft(g, 4, 0) == 0 && g[0] == "margaret" && g[3] == "rishi");
+    // assert(rotateLeft(g, 4, 3) == 3 && g[0] == "rishi" && g[3] == "theresa");
+    // assert(rotateLeft(g, 0, 2) == 2 && g[0] == "rishi" && g[2] == "liz");
+    assert(rotateLeft(g, 3, 4) == -1);
+
+    string e[4] = {"gordon", "tony", "", "john"};
+    assert(subsequence(h, 7, e, 4) == 2);
+
+    string d[5] = {"margaret", "margaret", "margaret", "tony", "tony"};
+    assert(countRuns(d, 5) == 2);
+    assert(countRuns(d, 3) == 1);
+    assert(countRuns(d, -1) == -1);
+    assert(countRuns(d, 0) == 0);
+
+    string d1[9] = {"tony", "boris", "rishi", "rishi", "gordon", "gordon", "gordon", "rishi", "rishi"};
+    assert(countRuns(d1, 9) == 5);
+
+    string d2[5] = {"gordon", "gordon", "gordon", "gordon", "gordon"};
+    assert(countRuns(d2, 5) == 1);
+
+
+    string f[3] = {"liz", "gordon", "tony"};
+    assert(flip(f, 3) == 3 && f[0] == "tony" && f[2] == "liz");
+
+
+
+
+
+    assert(lookupAny(h, 7, f, 3) == 2);
+    assert(split(h, 7, "liz") == 3);
+
+    cout << "All tests succeeded" << endl;
 }
